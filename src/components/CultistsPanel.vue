96ffa1cfdea52dd408be97a7007cafe89a406fc6
Tom <thomaslaycock01@gmail.com>
2024-06-18 18:54:21 +0100

    reworked cultist panel to now use a modal

--- src/components/CultistsPanel.vue
+++ src/components/CultistsPanel.vue
@@ -1,23 +1,116 @@
 <script setup>
-import Cultist from './Cultist.vue';
-
 import { useCultistsStore } from "@/stores/cultists";
 import { useMiscStore } from '@/stores/misc';
 
 const cultists = useCultistsStore();
 const misc = useMiscStore();
-
 </script>
 
 <template>
     <div class="title is-4 mb-1 segment-title">Cultists</div>
-    <!--<Cultist v-for="cultist in cultists.regularCultists" :cultistId="cultist.id"/>-->
     <div class="container">
         <span v-for="i in cultists.regularCultists">
-            <div class="button is-outlined">Cultist</div>
+            <button v-if="i.getFreeStatPoints() != 0"  class="button is-dark is-warning" @click="setNewActiveCultist(i)">{{i.getName()}}</button>
+            <button v-else class="button is-outlined" @click="setNewActiveCultist(i)">{{i.getName()}}</button>
         </span>
         <span v-for="i in misc.getCultistLimit - cultists.numOfCultists">
             <div class="button is-outlined">No cultist</div>
         </span>
     </div>
+
+    <section>
+        <b-modal
+            v-model="isCultistModalActive"
+            has-modal-card
+            trap-focus
+            :destroy-on-hide="false"
+            aria-role="dialog"
+            aria-label="cultistModal"
+            close-button-aria-label="Close"
+            ariaModal>
+            <div class="modal-card">
+                <header class="modal-card-head">
+                    <p class="modal-card-title">{{activeCultist.getName()}}</p>
+                    <button
+                            type="button"
+                            class="delete"
+                            @click="isCultistModalActive = false"></button>
+                </header>
+                <body class="modal-card-body">
+                    <div>{{ activeCultist.getJob() ? activeCultist.getJob() : "Unemployed" }}</div>
+                    <div>Level {{ activeCultist.getLevel() }}</div>
+                    <div>{{ activeCultist.getXp() }} / {{ activeCultist.getXpNeeded() }}XP</div>
+                    <div v-if="activeCultist.getFreeStatPoints() != 0">{{activeCultist.getFreeStatPoints()}} Skill Points Available</div>
+                    <table class="table is-striped is-narrow is-inline">
+                        <thead>
+                            <tr>
+                                <th>Stats</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Strength</td>
+                                <td>{{ activeCultist.getStat("str") }}</td>
+                                <td v-if="activeCultist.getFreeStatPoints() != 0"><button @click="addStr">+</button></td>
+                            </tr>
+                            <tr>
+                                <td>Intelligence</td>
+                                <td>{{ activeCultist.getStat("int") }}</td>
+                                <td v-if="activeCultist.getFreeStatPoints() != 0"><button @click="addInt">+</button></td>
+                            </tr>
+                            <tr>
+                                <td>Aglility</td>
+                                <td>{{ activeCultist.getStat("agi") }}</td>
+                                <td v-if="activeCultist.getFreeStatPoints() != 0"><button @click="addAgi">+</button></td>
+                            </tr>
+                            <tr>
+                                <td>Charisma</td>
+                                <td>{{ activeCultist.getStat("cha") }}</td>
+                                <td v-if="activeCultist.getFreeStatPoints() != 0"><button @click="addCha">+</button></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </body>
+                <footer class="modal-card-foot">
+                    <button
+                            type="button"
+                            class="button is-danger"
+                            @click="isCultistModalActive = false">Fire Cultist</button>
+                    </footer>
+            </div>
+        
+        </b-modal>
+    </section>
 </template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            isCultistModalActive: false,
+            activeCultist: {}
+        }
+    },
+    methods: {
+        setNewActiveCultist(cultist) {
+            this.activeCultist = cultist;
+            this.isCultistModalActive = true;
+        },
+        addStr() {
+            this.activeCultist.increaseStat("str");
+        },
+        addAgi() {
+            this.activeCultist.increaseStat("agi");
+        },
+        addInt() {
+            this.activeCultist.increaseStat("int");
+        },
+        addCha() {
+            this.activeCultist.increaseStat("cha");
+        }
+    }
+}
+
+</script>
