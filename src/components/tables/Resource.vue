4d68c0e3baef13b982b6df0b43eb50510d3f39e7
Tom <thomaslaycock01@gmail.com>
2024-07-25 11:10:10 +0100

    resource table now uses toLocaleString for readability

--- src/components/tables/Resource.vue
+++ src/components/tables/Resource.vue
@@ -8,7 +8,7 @@ const props = defineProps({
 <template>
     <tr v-if="props.object.showCondition()">
         <td>{{ props.resource }}</td>
-        <td>{{ props.object.total }}</td>
+        <td>{{ props.object.total.toLocaleString("en-GB") }}</td>
         <td>{{ props.object.perSec }} /s</td>
     </tr>
 </template>
