136e27a413054689319f55c545ef28384b3448de
Tom <thomaslaycock01@gmail.com>
2024-10-03 10:22:26 +0100

    added ActionList component to display actions on each tab

---
+++ src/components/ActionList.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { reactive } from 'vue';
+
+import Tooltip from './Tooltip.vue';
+
+
+const props = defineProps({
+    piniaObject: Object
+})
+
+
+var tooltip = reactive({id: null});
+
+function mouseEnterFunction(e) {
+    tooltip.id = e.target.value;
+}
+
+function mouseLeaveFunction(e) {
+    tooltip.id = null;
+}
+</script>
+
+
+<template>
+
+    <span v-for="action in props.piniaObject.getActions">
+        <div>
+            <div class="title is-5 mb-1 segment-title">{{ action.name }}</div>
+            <span v-for="button in action.buttons">
+                <button v-if="button.showCondition()" :disabled="!button.condition()" @click="button.effect" @mouseenter="mouseEnterFunction" @mouseleave="mouseLeaveFunction" :value="button.id" class="button is-dark mb-1 mr-2">{{ button.name }}</button>
+                <span v-if="tooltip.id == button.id">
+                    <Tooltip class="tooltip" :name="button.name" :desc="button.desc" :costs="button.costs"/>
+                </span>
+            </span>
+        </div>
+    </span>
+
+</template>
--- src/components/lairTab/LairTab.vue
+++ src/components/lairTab/LairTab.vue
@@ -1,39 +1,15 @@
 <script setup>
-import { reactive } from 'vue';
-
-import Tooltip from '../Tooltip.vue';
+import ActionList from '../ActionList.vue';
 
 import { useLairStore } from '@/stores/lair';
 
 const lair = useLairStore();
-
-var tooltip = reactive({id: null});
-
-function mouseEnterFunction(e) {
-    tooltip.id = e.target.value;
-}
-
-function mouseLeaveFunction(e) {
-    tooltip.id = null;
-}
 </script>
 
 <template>
 
     <div>
-        <span v-for="action in lair.getActions">
-            <div>
-                <div class="title is-5 mb-1 segment-title">{{ action.name }}</div>
-                <span v-for="button in action.buttons">
-                    <button v-if="button.showCondition()" :disabled="!button.condition()" @click="button.effect" @mouseenter="mouseEnterFunction" @mouseleave="mouseLeaveFunction" :value="button.id" class="button is-dark mb-1 mr-2">{{ button.name }}</button>
-                    <span v-if="tooltip.id == button.id">
-                        <Tooltip class="tooltip" :name="button.name" :desc="button.desc" :costs="button.costs"/>
-                    </span>
-                </span>
-            </div>
-            <!--
-            -->
-        </span>
+        <ActionList :piniaObject="lair"/>
     </div> 
 
 </template>
