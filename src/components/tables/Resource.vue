4f4f0529651e171538b662407a509e6d298f052c
Tom <thomaslaycock01@gmail.com>
2024-10-17 12:43:39 +0100

    added noPerSec property for smelted resources

--- src/components/tables/Resource.vue
+++ src/components/tables/Resource.vue
@@ -12,6 +12,6 @@ const props = defineProps({
     <tr v-if="!resources.checkIfLocked(props.object.id) && !resources.checkIfResourceHasProperty(props.object.id, 'alwaysLocked')">
         <td>{{ props.object.name }}</td>
         <td>{{ (Math.round(props.object.total * 100) / 100).toLocaleString("en-GB") }}</td>
-        <td>{{ Math.round(props.object.perSec * 100) / 100 }} /s</td>
+        <td v-if="!resources.checkIfResourceHasProperty(props.object.id, 'noPerSec')">{{ Math.round(props.object.perSec * 100) / 100 }} /s</td>
     </tr>
 </template>
