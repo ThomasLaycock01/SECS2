c46272b3f5f23ec86f34580e9e175791dd1b7fe7
Tom <thomaslaycock01@gmail.com>
2025-01-13 13:12:56 +0000

    simplified explore screen

--- src/components/barracksTab/ExploreTab.vue
+++ src/components/barracksTab/ExploreTab.vue
@@ -19,24 +19,14 @@ function deselectArea() {
 
 function startSettingParty() {
     exploreTab.settingParty = true;
-    if (exploreTab.selectedArea.getActiveParty()) {
-        exploreTab.selectedParty = exploreTab.selectedArea.getActiveParty();
-    }
 }
 
-function cancelSettingParty() {
-    exploreTab.selectedParty = null;
-    exploreTab.settingParty = false;
-}
-
-function confirmSelectedParty() {
-    exploreTab.selectedArea.setActiveParty(exploreTab.selectedParty);
-    exploreTab.selectedParty = null;
+function stopSettingParty() {
     exploreTab.settingParty = false;
 }
 
 function setSelectedParty(party) {
-    exploreTab.selectedParty = party;
+    exploreTab.selectedArea.setActiveParty(party);
 }
 </script>
 
@@ -53,22 +43,20 @@ function setSelectedParty(party) {
         <div v-if="exploreTab.settingParty">
             <div v-if="Object.keys(parties.getParties).length > 0">
                 <div v-for="i in parties.getParties">
-                    <button class="button is-dark" :class="exploreTab.selectedParty && i.getId() == exploreTab.selectedParty.getId() ? 'is-info' : ''" @click="setSelectedParty(i)">{{ i.getName() }}</button>
+                    <button class="button is-dark" :class="exploreTab.selectedArea.getActiveParty() && exploreTab.selectedArea.getActiveParty().getId() == i.getId() ? 'is-info' : ''" @click="setSelectedParty(i)">{{ i.getName() }}</button>
                 </div>
             </div>
             <div v-else>
                 No parties available!
             </div>
-            <button class="button is-dark" @click="confirmSelectedParty()">Confirm</button>
-            <button class="button is-danger" @click="cancelSettingParty()">Cancel</button>
+            <button class="button is-dark" @click="stopSettingParty()">Back</button>
         </div>
         <div v-else>
             <div>{{ exploreTab.selectedArea.getActiveParty() ? exploreTab.selectedArea.getActiveParty().getName() : "No party assigned!" }}</div>
             <button class="button is-dark"  @click="startSettingParty()">Set Party</button>
             <br>
             <br>
-            <button class="button is-dark">Embark!</button>
-            <button class="button is-danger" @click="deselectArea()">Cancel</button>
+            <button class="button is-dark" @click="deselectArea()">Back</button>
         </div>
 
     </div>
