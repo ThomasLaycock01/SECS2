7939dfc8c448a5cb1e627bece2a74baa47187cb8
Tom <thomaslaycock01@gmail.com>
2025-02-10 12:54:29 +0000

    added progression messages to the text log

--- src/stores/misc/progression.js
+++ src/stores/misc/progression.js
@@ -1,6 +1,9 @@
 import { defineStore } from "pinia";
 
 import { useResourcesStore } from "../globalPinias/resources";
+import { useCultistsStore } from "../globalPinias/cultists";
+
+import { useTextLogStore } from "./textLog";
 
 export const useProgressionStore = defineStore("progression", {
     state: () => {
@@ -9,19 +12,37 @@ export const useProgressionStore = defineStore("progression", {
             unlockConditions: {
                 "10Evilness": {
                     id: "10Evilness",
+                    TLMessage: true,
                     condition() {
                         const resources = useResourcesStore();
-                        return resources.checkIfCanAfford({"evilness": 10})
+                        return resources.checkIfCanAfford({"evilness": 10});
+                    }
+                },
+                "firstCultist": {
+                    id: "firstCultist",
+                    TLMessage: true,
+                    condition() {
+                        const cultists = useCultistsStore();
+                        return cultists.getNumOfRegular > 0;
                     }
                 }
             }
         }
     },
     getters: {
-
+        checkUnlocked(state) {
+            return (unlockId) => {
+                if (state.unlocked.includes(unlockId)) {
+                    return true;
+                }
+                return false;
+            }
+        }
     },
     actions: {
         updateProgression() {
+            const textLog = useTextLogStore();
+
             const unlockArray= Object.keys(this.unlockConditions);
 
             for (var i in unlockArray) {
@@ -30,6 +51,9 @@ export const useProgressionStore = defineStore("progression", {
 
                     if (conditionObj.condition()) {
                         this.unlocked.push(unlockArray[i]);
+                        if (conditionObj.TLMessage) {
+                            textLog.addMessageToLog(conditionObj.id);
+                        }
                     }
                 }
             }
