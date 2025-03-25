c87221ee482945a1db7aa467bbcb2c3250268661
Tom <thomaslaycock01@gmail.com>
2025-03-25 13:34:52 +0000

    insta heal now gated behind abandonded farmhouse

--- src/components/exploreTab/PartyTab.vue
+++ src/components/exploreTab/PartyTab.vue
@@ -5,23 +5,21 @@ import { usePartiesStore } from '@/stores/barracks/parties';
 import { useModalsStore } from '@/stores/misc/modal';
 import { useTooltipsStore } from '@/stores/misc/tooltips';
 import { useResourcesStore } from '@/stores/globalPinias/resources';
+import { useProgressionStore } from '@/stores/misc/progression';
 
 const parties = usePartiesStore();
 const modals = useModalsStore();
 const tooltips = useTooltipsStore();
 const resources = useResourcesStore();
+const progression = useProgressionStore();
 
 function instaHealClick(partyObj) {
-    const resources = useResourcesStore();
-
     resources.removeResources({grain:partyObj.getGrainHealCost()});
 
     partyObj.instaHealCultists();
 }
 
 function instaHealCheck(partyObj) {
-    const resources = useResourcesStore();
-
     if (partyObj.getGrainHealCost() == 0) {
         return false;
     }
@@ -46,7 +44,7 @@ function instaHealCheck(partyObj) {
                 {{ i.getName() }} - {{ i.getPartySize() }}/{{ i.getLimit() }} - Currently {{ i.getCurrentActivity() ? i.getCurrentActivity() : "doing nothing" }}
                 <button class="button is-dark" @click="modals.openParty(i)" :disabled="i.getCurrentActivity()">Edit</button>
                 <button class="button" :class="i.getIsHealing() ? 'is-danger' : 'is-info'" :disabled="i.getCurrentActivity() && i.getCurrentActivity() != 'Healing'" @click=i.toggleIsHealing()>{{i.getIsHealing() ? 'Stop' : 'Heal'}}</button>
-                <span>
+                <span v-if="progression.checkUnlocked('completedExpedition')">
                     <button class="button is-dark" @click="instaHealClick(i)" @mouseover="tooltips.setActiveTooltip(`instaHeal${i.getId()}`)" @mouseleave="tooltips.removeActiveTooltip()" :disabled="!instaHealCheck(i)">Insta-heal</button>
                     <span v-if="tooltips.getActiveTooltip == `instaHeal${i.getId()}`">
                         <Tooltip class="tooltip" tooltipType="regular" :tooltipObj="tooltips.getInstaHealTooltip(i)"/>
